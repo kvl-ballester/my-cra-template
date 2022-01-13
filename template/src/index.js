b7e8664bd8d25ab1e35ee5cf76706654db4a96bb
kvl-ballester <kvl.ballester@gmail.com>
2022-01-13 16:01:20 +0100

    exchange app.js for home page

--- template/src/index.js
+++ template/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { Home } from "./pages";
 
 import {
   BrowserRouter,
@@ -13,7 +13,7 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
